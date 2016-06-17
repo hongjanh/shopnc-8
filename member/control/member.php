6c7e1119da1429d24bbf446a987d9f3d8fc5799f
jetty.guo <shuaiguo_18@163.com>
2016-06-17 22:18:00 +0800

    提交

--- member/control/member.php
+++ member/control/member.php
@@ -4,10 +4,6 @@
  *
  *
  *
- * * @网店运维 (c) 2015-2018 ShopWWI Inc. (http://www.shopwwi.com)
- * @license    http://www.shopwwi.c om
- * @link       交流群号：111731672
- * @since      网店运维提供技术支持 授权请购买shopnc授权
  */
 
 
